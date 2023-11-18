82ffb75a3cf82fae6fb5c14c270d62c3b7ffb4d4
Berta González <bertagonzalezmartin@gmail.com>
2023-11-18 22:20:40 +0100

    Add router use and import

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,11 @@
 import morgan from "morgan";
 import express from "express";
 import app from "./app.js";
+import pingRouter from "../features/ping/router/pingRouter.js";
+import robocuteRouter from "../features/robocute/router/robocutesRouter.js";
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/", pingRouter);
+app.use("/robocutes", robocuteRouter);
