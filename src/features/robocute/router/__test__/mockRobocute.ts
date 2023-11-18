4654592d375f1a11f0f582f39298e69e35e58bb6
Berta González <bertagonzalezmartin@gmail.com>
2023-11-18 22:19:21 +0100

    Add router test and mock

---
+++ src/features/robocute/router/__test__/mockRobocute.ts
@@ -0,0 +1,28 @@
+import type { RoboCuteStructure } from "../../types";
+
+const robocutes: RoboCuteStructure[] = [
+  {
+    _id: "65589f628bc4e230ca98ce0e",
+    name: "Arturito",
+    movie: "Star Wars",
+    planet: "Naboo",
+    height: "108",
+    occupation: "Astromech droid",
+    image: "https://i.ibb.co/YcYqBXm/arturito.webp",
+    endurance: "4",
+    speed: "6",
+  },
+  {
+    _id: "65589fbc8bc4e230ca98ce10",
+    name: "C3PO",
+    movie: "Star Wars",
+    planet: "Tatooine",
+    height: "177",
+    occupation: "Protocol droid",
+    image: "https://i.ibb.co/X3LV2w4/c3p0.webp",
+    endurance: "3",
+    speed: "4",
+  },
+];
+
+export default robocutes;
