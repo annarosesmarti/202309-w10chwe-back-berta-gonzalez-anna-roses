e1da13b058ad1a64e561a245e0f32e2bf0ed45a6
Anna Roses <anna.roses.marti@gmail.com>
2023-11-18 15:31:17 +0100

    Add pingController method json message test

--- src/features/ping/__tests__/pingController.test.ts
+++ src/features/ping/__tests__/pingController.test.ts
@@ -3,19 +3,28 @@ import PingController from "../controller/pingController";
 
 describe("Given a PingController's getPong method", () => {
   describe("When it receives a response", () => {
+    const pingController = new PingController();
+    const mock = jest.fn().mockReturnThis();
+    const req = {};
+    const mockResponse: Pick<Response, "status" | "json"> = {
+      status: mock,
+      json: jest.fn(),
+    };
+
     test("Then it should call its method status with 200", () => {
-      const pingController = new PingController();
-      const mock = jest.fn().mockReturnThis();
-      const req = {};
-      const mockResponse: Pick<Response, "status" | "json"> = {
-        status: mock,
-        json: jest.fn(),
-      };
       const expectedStatusCode = 200;
 
       pingController.getPong(req as Request, mockResponse as Response);
 
       expect(mockResponse.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+
+    test("Then it should call its method json with a message 'message: '🏓''", () => {
+      const expectedMessage = { message: "🏓" };
+
+      pingController.getPong(req as Request, mockResponse as Response);
+
+      expect(mockResponse.json).toHaveBeenCalledWith(expectedMessage);
     });
   });
 });
