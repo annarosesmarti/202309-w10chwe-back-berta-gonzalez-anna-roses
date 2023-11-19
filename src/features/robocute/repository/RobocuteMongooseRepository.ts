d29cdfe28565b98b8a9a07bc1acc90fd8029ff21
Berta González <bertagonzalezmartin@gmail.com>
2023-11-19 18:13:38 +0100

    Fix getRobocute function at mongooseRepository

--- src/features/robocute/repository/RobocuteMongooseRepository.ts
+++ src/features/robocute/repository/RobocuteMongooseRepository.ts
@@ -2,11 +2,11 @@ import type { RoboCuteStructure, RobocutesRepository } from "../types.js";
 import Robocute from "../model/robocute.js";
 
 export class RobocuteMongooseRepository implements RobocutesRepository {
-  public getRobocutes = async (): Promise<RoboCuteStructure[]> => {
+  public async getRobocutes(): Promise<RoboCuteStructure[]> {
     const robocutes = await Robocute.find();
 
     return robocutes;
-  };
+  }
 }
 
 export default RobocuteMongooseRepository;
