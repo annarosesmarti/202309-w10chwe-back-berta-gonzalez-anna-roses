3b634d450560727c44c4fcc3b28fa3615abbade6
Anna Roses <anna.roses.marti@gmail.com>
2023-11-21 09:56:01 +0100

    Add UserController class and getUser method

---
+++ src/features/user/controller/UserController.ts
@@ -0,0 +1,27 @@
+import { type Response } from "express";
+import { type JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import { type UserCredentialStructure } from "../types";
+import type UserMongooseRepository from "../repository/userMongooseRepository.js";
+
+class UserController {
+  constructor(private readonly userRepository: UserMongooseRepository) {}
+
+  loginUser = async (
+    req: UserCredentialStructure,
+    res: Response,
+  ): Promise<void> => {
+    try {
+      const { username, password } = req.body;
+      const user = await this.userRepository.getUser(username, password);
+      const userData: JwtPayload = { sub: user._id, name: user.name };
+      const token = jwt.sign(userData, process.env.JWT_SECRET_KEY!);
+
+      res.status(200).json({ token: { token } });
+    } catch (error) {
+      res.status(401).json({ error: (error as Error).message });
+    }
+  };
+}
+
+export default UserController;
